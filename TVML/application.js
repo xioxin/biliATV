0c69f2196597290c12692472840a800a18aecdf6
xin <i@xioxin.com>
2017-11-19 09:16:54 +0800

    up

--- TVML/application.js
+++ TVML/application.js
@@ -10,7 +10,7 @@ var nowPlayer = null;
 *
 * */
 
-evaluateScripts(['http://127.0.0.1:80/biliATV/TVML/tvOS2.js'], function (success) {
+evaluateScripts([tvBaseURL+'/tvOS2.js'], function (success) {
 
   if (success) {
 
