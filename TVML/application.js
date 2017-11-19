7871a2f878f9737a2ada50ca878dba9a00b3077b
xin <i@xioxin.com>
2017-11-19 17:51:10 +0800

    up

--- TVML/application.js
+++ TVML/application.js
@@ -232,6 +232,7 @@ evaluateScripts([tvBaseURL+'/tvOS2.js'], function (success) {
             </section>
          </shelf>
          <shelf>
+         <title>周四</title>
             <section>
                <lockup>
                   <img src="path to images on your servers/Space_Movie/Space_Movie_250x375_B.png" width="182" height="274"></img>
