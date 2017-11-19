1e064577c7384e8d2f226f1931cdcb617e5f0528
xin <i@xioxin.com>
2017-11-19 17:50:13 +0800

    up

--- TVML/application.js
+++ TVML/application.js
@@ -245,6 +245,55 @@ evaluateScripts([tvBaseURL+'/tvOS2.js'], function (success) {
                   <img src="path to images on your server/Space_Movie/Space_Movie_250x375_C.png" width="182" height="274"></img>
                   <title>Movie 6</title>
                </lockup>
+               <lockup>
+                  <img src="path to images on your servers/Space_Movie/Space_Movie_250x375_B.png" width="182" height="274"></img>
+                  <title>Movie 4</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_A.png" width="182" height="274"></img>
+                  <title>Movie 5</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_C.png" width="182" height="274"></img>
+                  <title>Movie 6</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your servers/Space_Movie/Space_Movie_250x375_B.png" width="182" height="274"></img>
+                  <title>Movie 4</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_A.png" width="182" height="274"></img>
+                  <title>Movie 5</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_C.png" width="182" height="274"></img>
+                  <title>Movie 6</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your servers/Space_Movie/Space_Movie_250x375_B.png" width="182" height="274"></img>
+                  <title>Movie 4</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_A.png" width="182" height="274"></img>
+                  <title>Movie 5</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_C.png" width="182" height="274"></img>
+                  <title>Movie 6</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your servers/Space_Movie/Space_Movie_250x375_B.png" width="182" height="274"></img>
+                  <title>Movie 4</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_A.png" width="182" height="274"></img>
+                  <title>Movie 5</title>
+               </lockup>
+               <lockup>
+                  <img src="path to images on your server/Space_Movie/Space_Movie_250x375_C.png" width="182" height="274"></img>
+                  <title>Movie 6</title>
+               </lockup>
+               
             </section>
          </shelf>
       </collectionList>
