b0edb87f42811d3380c7cba7724965d39374076b
xin <i@xioxin.com>
2017-11-19 18:53:35 +0800

    up

--- TVML/application.js
+++ TVML/application.js
@@ -233,12 +233,12 @@ evaluateScripts([tvBaseURL+'/tvOS2.js'], function (success) {
                 <lockup binding="@onSelect:{select};" prototype="bangumi">
                     <img binding="@src:{cover};" width="200" height="300"/>
                     <title binding="textContent:{title};" />
-                    <description binding="textContent:{description};" />
+                    <description  binding="textContent:{description};" />
                 </lockup>
                 <lockup binding="@onSelect:{select};" prototype="bangumi_published">
                     <img binding="@src:{cover};" width="200" height="300"/>
                     <title binding="textContent:{title};" style="color:#fb7299" />
-                    <description>
+                    <description style="font-size: 30;color:#fff">
                         <span binding="textContent:{description};">111</span>
                         <span>222</span>
                     </description>
