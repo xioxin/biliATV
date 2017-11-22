07ff5abd7e37e0da07736e9b0069c1e074577a34
xin <i@xioxin.com>
2017-11-22 22:06:44 +0800

    up

--- TVML/application.js
+++ TVML/application.js
@@ -1253,7 +1253,7 @@ evaluateScripts([tvBaseURL+'/tvOS2.js'], function (success) {
    </stackTemplate>
 </document>`);
         test.temp = view;
-        test.view = view.view;
+        test.view = view.view.getElementsByTagName("section").item(0);
         // view.dataItem.title = "测试标题";
 
         // test.section = view.getElementsByTagName("section").item(0);
